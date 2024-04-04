df69c0ea2ddc2e5b40f642edf080c50b91d39e6b
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-04-04 00:51:53 -0300

    feat(tailwind): adiciona cores e estilos base personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#D9DBE9',
+        'body': '#1F1F1F',
+        'title': '#0A0A0A'
+      }
+    },
   },
   plugins: [],
 }
