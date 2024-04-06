ce969ba7202162552cfedac7fea3b225b21d44a5
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-04-06 12:25:08 -0300

    feat(tailwind): adiciona imagens de modelos para hero mobile e atualiza fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
     extend: {
       colors: {
         'background': '#F5F5F5',
-        'body': '#1F1F1F',
+        'body': '#212222',
         'title': '#0A0A0A',
       },
+      fontFamily: {
+        'garamond': ["EB Garamond", "serif"]
+      },
       backgroundImage: {
-        'hero-mobile': "url('/src/assets/img/hero/hero-mobile.jpg')",
+        'hero-mobile-1': "url('/src/assets/img/hero/hero-mobile-model1.jpg')",
+        'hero-mobile-2': "url('/src/assets/img/hero/hero-mobile-model2.jpg')",
+        'hero-mobile-3': "url('/src/assets/img/hero/hero-mobile-model3.jpg')",
       }
     },
   },
