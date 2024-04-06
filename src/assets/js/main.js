aab0eb6d07bfe3a1aa0ac9a44348dd609f8adb31
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-04-06 17:30:29 -0300

    feat: adiciona lógica para alterar cor de fundo dos botões ao clicar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,15 +5,26 @@ if (heroButtons) {
     const buttonsToChangeHero = Array.from(heroButtons?.children);
     function handleChangeHeroImages(index) {
         heroSection?.classList.remove('bg-hero-mobile-1', 'bg-hero-mobile-2', 'bg-hero-mobile-3');
+        buttonsToChangeHero.forEach((btn) => {
+            btn.classList.add('bg-background/40');
+            btn.classList.remove('bg-background');
+        });
+        function toggleHeroButtonColor() {
+            buttonsToChangeHero[index].classList.remove('bg-background/40');
+            buttonsToChangeHero[index].classList.add('bg-background');
+        }
         switch (index) {
             case 0:
                 heroSection?.classList.add('bg-hero-mobile-1');
+                toggleHeroButtonColor();
                 break;
             case 1:
                 heroSection?.classList.add('bg-hero-mobile-2');
+                toggleHeroButtonColor();
                 break;
             case 2:
                 heroSection?.classList.add('bg-hero-mobile-3');
+                toggleHeroButtonColor();
                 break;
         }
     }
--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -6,16 +6,28 @@ if (heroButtons) {
   
   function handleChangeHeroImages(index: number) {
     heroSection?.classList.remove('bg-hero-mobile-1', 'bg-hero-mobile-2', 'bg-hero-mobile-3')
+    buttonsToChangeHero.forEach((btn) => {
+      btn.classList.add('bg-background/40')
+      btn.classList.remove('bg-background')
+    })
+    
+    function toggleHeroButtonColor() {
+      buttonsToChangeHero[index].classList.remove('bg-background/40')
+      buttonsToChangeHero[index].classList.add('bg-background')
+    }
     
     switch(index) {
       case 0:
       heroSection?.classList.add('bg-hero-mobile-1')
+      toggleHeroButtonColor()
       break
       case 1:
       heroSection?.classList.add('bg-hero-mobile-2')
+      toggleHeroButtonColor()
       break
       case 2:
       heroSection?.classList.add('bg-hero-mobile-3')
+      toggleHeroButtonColor()
       break
     }
   }
