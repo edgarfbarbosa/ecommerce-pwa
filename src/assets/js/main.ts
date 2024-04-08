59ede9676723a26c394a29c1dabf0e972235abe2
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-04-08 09:46:58 -0300

    refactor: separa lógica de mudança de imagens do hero

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,38 +1,12 @@
-const heroSection = document.getElementById('heroSection')
-const heroButtons = document.getElementById('heroButtons')
+import handleChangeHeroImages from './modules/handleChangeHeroImages.js';
 
-if (heroButtons) {
-  const buttonsToChangeHero = Array.from(heroButtons?.children)
+document.addEventListener('DOMContentLoaded', () => {
+  const heroButtons = document.getElementById('heroButtons')
   
-  function handleChangeHeroImages(index: number) {
-    heroSection?.classList.remove('bg-hero-mobile-1', 'bg-hero-mobile-2', 'bg-hero-mobile-3')
-    buttonsToChangeHero.forEach((btn) => {
-      btn.classList.add('bg-background/40')
-      btn.classList.remove('bg-background')
+  if (heroButtons){
+    const buttonsToChangeHero = Array.from(heroButtons.children)
+    buttonsToChangeHero.forEach((btn, index) => {
+      btn.addEventListener('click', () => handleChangeHeroImages(index))
     })
-    
-    function toggleHeroButtonColor() {
-      buttonsToChangeHero[index].classList.remove('bg-background/40')
-      buttonsToChangeHero[index].classList.add('bg-background')
-    }
-    
-    switch(index) {
-      case 0:
-      heroSection?.classList.add('bg-hero-mobile-1')
-      toggleHeroButtonColor()
-      break
-      case 1:
-      heroSection?.classList.add('bg-hero-mobile-2')
-      toggleHeroButtonColor()
-      break
-      case 2:
-      heroSection?.classList.add('bg-hero-mobile-3')
-      toggleHeroButtonColor()
-      break
-    }
   }
-  
-  buttonsToChangeHero.forEach((btn, index) => {
-    btn.addEventListener('click', () => handleChangeHeroImages(index))
-  })
-}
+})
---
+++ src/assets/js/modules/handleChangeHeroImages.ts
@@ -0,0 +1,33 @@
+export default function handleChangeHeroImages(index: number) {
+  const heroSection = document.getElementById('heroSection')
+  const heroButtons = document.getElementById('heroButtons')
+  
+  if (heroSection && heroButtons) {
+    const buttonsToChangeHero = Array.from(heroButtons.children)
+    heroSection.classList.remove('bg-hero-mobile-1', 'bg-hero-mobile-2', 'bg-hero-mobile-3')
+    buttonsToChangeHero.forEach((btn) => {
+      btn.classList.add('bg-background/40')
+      btn.classList.remove('bg-background')
+    })
+    
+    function toggleHeroButtonColor(button: number) {
+      buttonsToChangeHero[button].classList.remove('bg-background/40')
+      buttonsToChangeHero[button].classList.add('bg-background')
+    }
+    
+    switch(index) {
+      case 0:
+      heroSection.classList.add('bg-hero-mobile-1')
+      toggleHeroButtonColor(0)
+      break
+      case 1:
+      heroSection.classList.add('bg-hero-mobile-2')
+      toggleHeroButtonColor(1)
+      break
+      case 2:
+      heroSection.classList.add('bg-hero-mobile-3')
+      toggleHeroButtonColor(2)
+      break
+    }
+  }
+}
